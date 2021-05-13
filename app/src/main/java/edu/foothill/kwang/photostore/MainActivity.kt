da5f84896dd46e564faae1d69e02b3ed3d5edfcd
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-05-12 19:13:24 -0700

    add onclicklistener for button to open image picker

--- app/src/main/java/edu/foothill/kwang/photostore/MainActivity.kt
+++ app/src/main/java/edu/foothill/kwang/photostore/MainActivity.kt
@@ -1,12 +1,30 @@
 package edu.foothill.kwang.photostore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val PICK_PHOTO_CODE = 4
+        val choose_btn = findViewById<Button>(R.id.btn_choose_img)
+        choose_btn.setOnClickListener {
+            Log.i("Main", "Open image picker")
+            val imagePickerIntent = Intent(Intent.ACTION_GET_CONTENT)
+            imagePickerIntent.type = "image/*"
+            if (imagePickerIntent.resolveActivity(packageManager) !== null) {
+                startActivityForResult(imagePickerIntent, PICK_PHOTO_CODE)
+            }
 
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
